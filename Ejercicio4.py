e149a7fe91694b7b3202f3c438ab19a803a34447
Nacho <isanccal@myuax.com>
2023-01-19 12:24:14 +0100

    Cambio 2

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,12 +1,21 @@
 import re 
 from operator import itemgetter
 from decimal import Decimal, ROUND_HALF_UP
-def do_math(a, b, operator):
-    if operator == '+':
-        return a + b
-    elif operator == '-':
-        return a - b
-    elif operator == '*':
-        return a * b
-    elif operator == '/':
-        return a / b
+def do_math(string):
+    numbers = string.split()
+    num_dict ={}
+    for number in numbers:
+        letter = re.search("[a-zA-Z]",number).group()
+        num_dict[number] = letter
+
+    num_dict = sorted(num_dict.items(), key=itemgetter(1))
+    result = None
+    for num in num_dict:
+        if result == None:
+            result = Decimal(num[0])
+        else:
+            result = result * Decimal(num[0])
+    return result.quantize(Decimal('1'), rounding=ROUND_HALF_UP)
+    
+
+ 
