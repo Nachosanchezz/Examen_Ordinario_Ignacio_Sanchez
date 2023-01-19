1f7bb84c768d24eef6959f0427403f5ca9de20c9
Nacho <isanccal@myuax.com>
2023-01-19 11:32:29 +0100

    Cambio 2

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,8 +1,24 @@
-class nonogram: 
-    def __init__(self, columns, rows):
-    
-        columns = ((1, 1, 0, 0, 0), (4, 0, 0, 0, 0), (1, 1, 1, 0, 0), (3, 0, 0, 0, 0), (1, 0, 0, 0, 0))
-        rows = ((1, 0, 0, 0, 0), (2, 0, 0, 0, 0), (3, 0, 0, 0, 0), (2, 1, 0, 0, 0) (4, 0, 0, 0, 0))
+class Nonogram: 
+    def __init__(self, clues_columns, clues_rows):
+        self.clues_columns = clues_columns
+        self.clues_rows = clues_rows
+
+        clues_columns = ((1, 1, 0, 0, 0), (4, 0, 0, 0, 0), (1, 1, 1, 0, 0), (3, 0, 0, 0, 0), (1, 0, 0, 0, 0))
+        clues_rows = ((1, 0, 0, 0, 0), (2, 0, 0, 0, 0), (3, 0, 0, 0, 0), (2, 1, 0, 0, 0), (4, 0, 0, 0, 0))
+
+        pass
+
+    def solve(self):
+
+        pass
+       
+
+
+ 
+
+
+   
+
         
 
 
