fef9821863dc6729d260ee1e346b00409c987d05
Nacho <isanccal@myuax.com>
2023-01-19 12:02:37 +0100

    Cambio final

--- Ejercicio10.py
+++ Ejercicio10.py
@@ -3,4 +3,13 @@ def tribonacci(n):
         return 0
     elif n==1:
         return 1
+    elif n==2:
+        return 1
+    else:
+        a,b,c=0,1,1
+        for i in range(3,n+1):
+            a,b,c=b,c,a+b+c
+        return c
+
+
     
