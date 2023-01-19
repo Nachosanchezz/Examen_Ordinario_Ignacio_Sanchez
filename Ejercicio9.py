a408910923679316fa0bdaa9d306650338ee94d2
Nacho <isanccal@myuax.com>
2023-01-19 12:37:37 +0100

    Cambio 1

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -4,20 +4,26 @@ def verificar_edad_trabajar(edad):
     else:
         return False
 
-def calcular_lunes(fecha_cumpleanios, fecha_actual):
-    dias_entre_fechas = (fecha_actual - fecha_cumpleanios).days
+def calcular_lunes(fecha_cumpleaños, fecha_actual):
+    dias_entre_fechas = (fecha_actual - fecha_cumpleaños).days
     lunes = dias_entre_fechas // 7
     return lunes
 
-def numero_lunes(fecha_cumpleanios):
+def numero_lunes(fecha_cumpleaños):
     hoy = datetime.datetime.now()
-    edad = calcular_edad(fecha_cumpleanios)
+    edad = calcular_edad(fecha_cumpleaños)
     trabajar = verificar_edad_trabajar(edad)
     if trabajar:
-        lunes = calcular_lunes(fecha_cumpleanios, hoy)
+        lunes = calcular_lunes(fecha_cumpleaños, hoy)
         return lunes
     else:
         return 0
+
+fecha_cumpleaños = datetime.datetime(1998, 12, 12)
+print(numero_lunes(fecha_cumpleaños))
+
+   
+
 
 
 
